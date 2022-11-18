3e8270fb59d47edc782bc64260ede479c943468f
Joe Hladnik <jhladnik20@gmail.com>
2022-11-18 17:52:13 -0600

    Add lFunctionToLowerCase from 11.18.2022

---
+++ lFunctionToLowerCase.js
@@ -0,0 +1,11 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+var toLowerCase = function(s) {
+    return s.toLowerCase()
+};
+
+//given a string of letters
+//return the string with entirely lowercase
+//input("Hello"), output("hello");
+//input("here"), output("here");
+//input("LOVELY"), output("lovely")
