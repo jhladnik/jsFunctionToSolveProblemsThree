ef5d1a00eb3315d3491b341ea9637df34e74de77
Joe Hladnik <jhladnik20@gmail.com>
2023-04-19 19:02:32 -0500

    Add functionPartsSum from 04.19.2023

---
+++ functionPartsSum.js
@@ -0,0 +1,39 @@
+// Let us consider this example (array written in general format):
+
+// ls = [0, 1, 3, 6, 10]
+
+// Its following parts:
+
+// ls = [0, 1, 3, 6, 10]
+// ls = [1, 3, 6, 10]
+// ls = [3, 6, 10]
+// ls = [6, 10]
+// ls = [10]
+// ls = []
+// The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+// The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+
+function partsSums(ls) {
+    //create an empty array to hold the sums; loop through ls; create a slice for each and sum the slice;
+    //push sum to empty array; push a zero element after loop and return
+    let sums = [];
+    
+    for(let i=0; i<ls.length; i++){
+      let cutUp = ls.slice(i);
+      let sumCutUp = 0;
+      for(let j=0; j<cutUp.length; j++){
+        sumCutUp += cutUp[j]
+      }
+      sums.push(sumCutUp);
+    };
+    sums.push(0);
+    return sums
+}
+
+//given an array of integers
+//return an array of integers that is compliled of the sums of the array but for each element in the array
+//the array should be summed to the left, creating an array.length of array.length+1
+//console.log(partsSums([0, 1, 3, 6, 10]), [20, 20, 19, 16, 10, 0]);
+//console.log(partsSums([1, 2, 3, 4, 5, 6]), [21, 20, 18, 15, 11, 6, 0]);
+//console.log(partsSums([744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]), [10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]);
